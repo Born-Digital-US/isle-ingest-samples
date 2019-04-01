b6815e23a045f662bf812bdd2ba8303c75d1afdf
Noah Smith <noah.smith@commonmediainc.com>
2019-04-01 10:07:14 -0400

    param to allow newspapers only to prevent timeout

--- Batches-by-CModel/ingest_samples.sh
+++ Batches-by-CModel/ingest_samples.sh
@@ -10,6 +10,7 @@ if [  -z "$1" ]; then
 fi
 
 DRUPAL_ROOT=$1
+RUN_NEWSPAPERS_ONLY=$2
 
 if [ ! -d "$DRUPAL_ROOT" ]; then
     echo "The path provided for the drupal root does not exist"
@@ -22,52 +23,55 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd $DRUPAL_ROOT
 
-echo "Ingesting the \"ICG Samples\" collection to hold everything else."
-drush -u 1 islandora_batch_with_derivs_preprocess --key_datastream=MODS --scan_target=$SCRIPT_DIR/Collections/files --use_pids=true --namespace=samples --parent=islandora:root --content_models=islandora:collectionCModel
-drush -u 1 islandora_batch_ingest
+if [ "1" != "$RUN_NEWSPAPERS_ONLY" ]; then
+  echo "Ingesting the \"ICG Samples\" collection to hold everything else."
+  drush -u 1 islandora_batch_with_derivs_preprocess --key_datastream=MODS --scan_target=$SCRIPT_DIR/Collections/files --use_pids=true --namespace=samples --parent=islandora:root --content_models=islandora:collectionCModel
+  drush -u 1 islandora_batch_ingest
 
-echo "Ingesting the newspaper object which needs to exist before ingesting issues."
-drush -u 1 islandora_batch_with_derivs_preprocess --key_datastream=MODS --scan_target=$SCRIPT_DIR/Newspaper/files --use_pids=true --namespace=samples --parent=samples:collection --content_models=islandora:newspaperCModel
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting the newspaper object which needs to exist before ingesting issues."
+  drush -u 1 islandora_batch_with_derivs_preprocess --key_datastream=MODS --scan_target=$SCRIPT_DIR/Newspaper/files --use_pids=true --namespace=samples --parent=samples:collection --content_models=islandora:newspaperCModel
+  drush -u 1 islandora_batch_ingest
 
-echo "Ingesting audio content"
-drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/audioCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp-audioCModel
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting audio content"
+  drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/audioCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp-audioCModel
+  drush -u 1 islandora_batch_ingest
 
-echo "Ingesting basic image content"
-drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/basicImageCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_basic_image
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting basic image content"
+  drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/basicImageCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_basic_image
+  drush -u 1 islandora_batch_ingest
 
-echo "Ingesting book content"
-drush -u 1 islandora_book_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/bookCModel/files --namespace=samples --parent=samples:collection
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting book content"
+  drush -u 1 islandora_book_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/bookCModel/files --namespace=samples --parent=samples:collection
+  drush -v -u 1 islandora_batch_ingest
 
-echo "Ingesting compound content"
-drush -u 1 islandora_compound_batch_preprocess --scan_target=$SCRIPT_DIR/compoundCModel/files --namespace=samples --parent=samples:collection
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting compound content"
+  drush -u 1 islandora_compound_batch_preprocess --scan_target=$SCRIPT_DIR/compoundCModel/files --namespace=samples --parent=samples:collection
+  drush -u 1 islandora_batch_ingest
 
-echo "Ingesting large images"
-drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/largeImageCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_large_image_cmodel
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting large images"
+  drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/largeImageCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_large_image_cmodel
+  drush -u 1 islandora_batch_ingest
 
-echo "Ingesting pdf content"
-drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/pdfCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_pdf
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting pdf content"
+  drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/pdfCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_pdf
+  drush -u 1 islandora_batch_ingest
 
-echo "Ingesting video content"
-drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/videoCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_videoCModel
-drush -u 1 islandora_batch_ingest
+  echo "Ingesting video content"
+  drush -u 1 islandora_batch_scan_preprocess --type=directory --scan_target=$SCRIPT_DIR/videoCModel/files --namespace=samples --parent=samples:collection --content_models=islandora:sp_videoCModel
+  drush -u 1 islandora_batch_ingest
 
-echo "Pre-processing newspaper issues. This may take a while..."
-echo "Vol 1 No 7"
-drush -u 1 islandora_newspaper_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/newspaperIssues/files1 --namespace=samples --parent=samples:newspaper --content_models=islandora:newspaperIssueCModel --aggregate_ocr
-echo "Newspaper batch Vol 1 No 7 pre-processed. Ingesting..."
-drush -v -u 1 islandora_batch_ingest
-echo "Vol 1 No 8"
-drush -u 1 islandora_newspaper_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/newspaperIssues/files2 --namespace=samples --parent=samples:newspaper --content_models=islandora:newspaperIssueCModel --aggregate_ocr
-echo "Newspaper batch Vol 1 No 8 pre-processed. Ingesting..."
-drush -v -u 1 islandora_batch_ingest
-echo "Vol 2 No 7"
-drush -u 1 islandora_newspaper_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/newspaperIssues/files3 --namespace=samples --parent=samples:newspaper --content_models=islandora:newspaperIssueCModel --aggregate_ocr
-echo "Newspaper batch Vol 2 No 7 pre-processed. Ingesting..."
-drush -v -u 1 islandora_batch_ingest
+else
+  echo "Pre-processing newspaper issues. This may take a while..."
+  echo "Vol 1 No 7"
+  drush -u 1 islandora_newspaper_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/newspaperIssues/files1 --namespace=samples --parent=samples:newspaper --content_models=islandora:newspaperIssueCModel --aggregate_ocr
+  echo "Newspaper batch Vol 1 No 7 pre-processed. Ingesting..."
+  drush -v -u 1 islandora_batch_ingest
+  echo "Vol 1 No 8"
+  drush -u 1 islandora_newspaper_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/newspaperIssues/files2 --namespace=samples --parent=samples:newspaper --content_models=islandora:newspaperIssueCModel --aggregate_ocr
+  echo "Newspaper batch Vol 1 No 8 pre-processed. Ingesting..."
+  drush -v -u 1 islandora_batch_ingest
+  echo "Vol 2 No 7"
+  drush -u 1 islandora_newspaper_batch_preprocess --type=directory --scan_target=$SCRIPT_DIR/newspaperIssues/files3 --namespace=samples --parent=samples:newspaper --content_models=islandora:newspaperIssueCModel --aggregate_ocr
+  echo "Newspaper batch Vol 2 No 7 pre-processed. Ingesting..."
+  drush -v -u 1 islandora_batch_ingest
+fi
